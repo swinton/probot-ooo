7178241c3a865a8de4f12b38acad3b0ded5a45bf
Steve Winton <stevewinton@gmail.com>
2018-01-04 05:25:41 -0600

    Use sherlockjs for OOO start and end parsing

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+// Use UTC for for all Date parsing
+process.env.TZ = 'UTC';
+
 const commands = require('probot-commands')
+const Sherlock = require('sherlockjs')
 
 module.exports = robot => {
   // Type `/ooo TBD` in a comment box for an Issue or Pull Request
   const keyword = 'ooo'
   const handler = (context, command) => {
-    console.log(command)
+    let event = Sherlock.parse(command.arguments)
+    console.log('startDate', event.startDate)
+    console.log('endDate', event.endDate)
   }
-  const command = new commands.Command(keyword, handler)
 
-  robot.on('issues.opened', command.listener.bind(command))
   commands(robot, keyword, handler)
+
+  // TODO
+  // Get this to work for issues.opened as well
+  // const command = new commands.Command(keyword, handler)
+  // robot.on('issues.opened', command.listener.bind(command))
 }
