0b76c97db51bcbba815932feb7269043dd974213
Steve Winton <stevewinton@gmail.com>
2018-01-03 18:39:18 -0600

    Install and configure probot-commands

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-module.exports = (robot) => {
+const commands = require('probot-commands')
 
-  robot.on('issues.opened', async context => {
-    console.log('issue opened', context.payload)
-  })
+module.exports = robot => {
+  // Type `/ooo TBD` in a comment box for an Issue or Pull Request
+  const keyword = 'ooo'
+  const handler = (context, command) => {
+    console.log(command)
+  }
+  const command = new commands.Command(keyword, handler)
 
+  robot.on('issues.opened', command.listener.bind(command))
+  commands(robot, keyword, handler)
 }
