a563e564af96539293380ee2b18015de772735fa
Steve Winton <stevewinton@gmail.com>
2018-01-04 05:52:10 -0600

    Use moment to format a response

--- index.js
+++ index.js
@@ -3,14 +3,33 @@ process.env.TZ = 'UTC';
 
 const commands = require('probot-commands')
 const Sherlock = require('sherlockjs')
+const moment = require('moment')
 
 module.exports = robot => {
-  // Type `/ooo TBD` in a comment box for an Issue or Pull Request
+  // Type `/ooo from to until` in a comment box for an Issue or Pull Request
   const keyword = 'ooo'
   const handler = (context, command) => {
-    let event = Sherlock.parse(command.arguments)
-    console.log('startDate', event.startDate)
-    console.log('endDate', event.endDate)
+    let ooo = Sherlock.parse(command.arguments),
+        sender = context.payload.sender.login,
+        owner = context.payload.repository.owner.login,
+        repo = context.payload.repository.name,
+        comment_id = context.payload.comment.id,
+        comment_html_url = context.payload.comment.html_url
+
+    // TODO
+    // Preserve the following data
+    console.log('startDate', ooo.startDate)
+    console.log('endDate', ooo.endDate)
+    console.log('sender', sender)
+    console.log('owner', owner)
+    console.log('repo', repo)
+    console.log('comment_id', comment_id)
+    console.log('comment_html_url', comment_html_url)
+
+    // Respond with confirmation
+    context.github.issues.createComment(context.issue({
+      body: `:+1: Marked @${sender} as [OOO from ${moment(ooo.startDate).format("dddd, MMMM Do YYYY")} to ${moment(ooo.endDate).format("dddd, MMMM Do YYYY")}](${comment_html_url}) :calendar:.`
+    }))
   }
 
   commands(robot, keyword, handler)
