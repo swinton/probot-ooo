d650c78e36021a766f1f92aed6aa97ff6ec04180
Steve Winton <stevewinton@gmail.com>
2018-01-03 18:02:52 -0600

    Simulate opened issue

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ npm install
 
 # Run the bot
 npm start
+
+# Simulate an opened issue
+npm run-script simulate
 ```
 
 See [docs/deploy.md](docs/deploy.md) if you would like to run your own instance of this app.
--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 module.exports = (robot) => {
-  // Your code here
-  console.log('Yay, the app was loaded!')
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  robot.on('issues.opened', async context => {
+    console.log('issue opened', context.payload)
+  })
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
